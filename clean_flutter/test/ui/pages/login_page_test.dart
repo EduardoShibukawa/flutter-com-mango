1b82b88105c3f033151036351ef947f4dcc440cb
Eduardo Shibukawa <eduardo.shibukawa@thoughtworks.com>
2021-07-15 10:59:01 -0300

    ensure LoginPage hide loading

--- clean_flutter/test/ui/pages/login_page_test.dart
+++ clean_flutter/test/ui/pages/login_page_test.dart
@@ -51,6 +51,7 @@ void main() {
     final button = tester.widget<ElevatedButton>(find.byType(ElevatedButton));
 
     expect(button.onPressed, null);
+    expect(find.byType(CircularProgressIndicator), findsNothing);
   });
 
   testWidgets('Should call validate with correct values',
@@ -150,6 +151,18 @@ void main() {
     await tester.pump();
 
     expect(find.byType(CircularProgressIndicator), findsOneWidget);
+  });
+
+  testWidgets('should hide loading', (WidgetTester tester) async {
+    await loadPage(tester);
+
+    isLoadingController.add(true);
+    await tester.pump();
+
+    isLoadingController.add(false);
+    await tester.pump();
+
+    expect(find.byType(CircularProgressIndicator), findsNothing);
   });
 }
 
