301cfad5685a43f57dfa847b9588344e01d5f220
Eduardo Shibukawa <eduardo.shibukawa@thoughtworks.com>
2021-07-15 18:33:47 -0300

    ensure RequiredFieldValidation return error if value os empty

--- clean_flutter/test/validation/validators/required_field_validation_test.dart
+++ clean_flutter/test/validation/validators/required_field_validation_test.dart
@@ -1,12 +1,20 @@
 import 'package:test/test.dart';
 
 void main() {
-  test('should return empty string if value is not empty', () {
+  test('should return empty error if value is not empty', () {
     final sut = RequiredFieldValidation('any_field');
 
     final error = sut.validate('any_value');
 
     expect(error, '');
+  });
+
+  test('should return error if value is empty', () {
+    final sut = RequiredFieldValidation('any_field');
+
+    final error = sut.validate('');
+
+    expect(error, 'Campo obrigatório!');
   });
 }
 
@@ -22,6 +30,6 @@ class RequiredFieldValidation implements FIeldValidation {
 
   @override
   String validate(String value) {
-    return '';
+    return value.isEmpty ? 'Campo obrigatório!' : '';
   }
 }
