bd433d93ee46b783d9a8fa272703243b8f5df48f
Eduardo Shibukawa <eduardo.shibukawa@thoughtworks.com>
2021-07-15 13:53:41 -0300

    move Authentication mock to a helper method

--- clean_flutter/test/ui/presentation/presenters/stream_login_presenter_test.dart
+++ clean_flutter/test/ui/presentation/presenters/stream_login_presenter_test.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:faker/faker.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
@@ -9,14 +11,20 @@ void main() {
   late Validation validation;
   late String email;
 
+  When mockValidationCall({String? field}) => when(() => validation.validate(
+        field: field ?? any(named: 'field'),
+        value: any(named: 'value'),
+      ));
+
+  void mockValidation({String? field, String? value}) {
+    mockValidationCall(field: field).thenReturn(value ?? '');
+  }
+
   setUp(() {
     validation = ValidationSpy();
     sut = StreamLoginPresenter(validation: validation);
     email = faker.internet.email();
-    when(() => validation.validate(
-          field: any(named: 'field'),
-          value: any(named: 'value'),
-        )).thenAnswer((_) => '');
+    mockValidation();
   });
 
   test('Should call Validation with correct email', () {
@@ -24,18 +32,38 @@ void main() {
 
     verify(() => validation.validate(field: 'email', value: email)).called(1);
   });
+
+  test('Should emit email error if validation fails', () {
+    mockValidation(value: 'error');
+
+    expectLater(sut.emailErrorStream, emits('error'));
+
+    sut.validaEmail(email);
+  });
 }
 
 abstract class Validation {
   String validate({required String field, required String value});
 }
 
+class LoginState {
+  late String emailError;
+}
+
 class StreamLoginPresenter {
   final Validation validation;
+  final _controller = StreamController<LoginState>.broadcast();
+
+  var _state = LoginState();
+
+  Stream<String> get emailErrorStream =>
+      _controller.stream.map((state) => state.emailError);
 
   StreamLoginPresenter({required this.validation});
 
   void validaEmail(String email) {
-    validation.validate(field: 'email', value: email);
+    _state.emailError = validation.validate(field: 'email', value: email);
+
+    _controller.add(_state);
   }
 }
