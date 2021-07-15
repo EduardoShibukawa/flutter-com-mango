60271b919a3bc73cb311fc4e50f72d7efadd1800
Eduardo Shibukawa <eduardo.shibukawa@thoughtworks.com>
2021-07-15 18:59:02 -0300

    ensure EmailValidation return error if email is invalid

--- clean_flutter/test/validation/validators/email_validation_test.dart
+++ clean_flutter/test/validation/validators/email_validation_test.dart
@@ -19,6 +19,10 @@ void main() {
   test('Should return blank if email is valid', () {
     expect(sut.validate('eduardoshibuka@gmail.com'), '');
   });
+
+  test('Should return error if email is invalid', () {
+    expect(sut.validate('eduardoshibuka@gmail'), 'Campo inválido!');
+  });
 }
 
 class EmailValidation implements FIeldValidation {
@@ -27,6 +31,13 @@ class EmailValidation implements FIeldValidation {
   EmailValidation(this.field);
 
   String validate(String? value) {
+    final regex = RegExp(
+        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+
+    if (value?.isNotEmpty == true && !regex.hasMatch(value!)) {
+      return 'Campo inválido!';
+    }
+
     return '';
   }
 }
