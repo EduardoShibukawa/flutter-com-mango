39127c18f7a6ac7ba40e07bc18275fa16141bdf7
Eduardo Shibukawa <eduardo.shibukawa@thoughtworks.com>
2021-07-16 09:32:58 -0300

    move duplicate code to setUp method

--- clean_flutter/test/validation/validators/validation_composite.dart
+++ clean_flutter/test/validation/validators/validation_composite.dart
@@ -4,15 +4,41 @@ import 'package:test/test.dart';
 import 'package:clean_flutter/presentation/presents/protocols/protocols.dart';
 import 'package:clean_flutter/validation/protocols/protocols.dart';
 
-class FieldValidationSpy extends Mock implements FIeldValidation {};
+class FieldValidationSpy extends Mock implements FIeldValidation {}
+
 void main() {
+  late ValidationComposite sut;
+  late FIeldValidation validation1;
+  late FIeldValidation validation2;
+  late FIeldValidation validation3;
+
+  mockValidation1(String error) {
+    when(() => validation1.field).thenReturn('');
+  }
+
+  mockValidation2(String error) {
+    when(() => validation2.field).thenReturn('');
+  }
+
+  mockValidation3(String error) {
+    when(() => validation3.field).thenReturn('');
+  }
+
+  setUp(() {
+    validation1 = FieldValidationSpy();
+    validation2 = FieldValidationSpy();
+    validation3 = FieldValidationSpy();
+    sut = ValidationComposite([validation1, validation2, validation3]);
+
+    when(() => validation1.field).thenReturn('any_field');
+    when(() => validation2.field).thenReturn('any_field');
+    when(() => validation3.field).thenReturn('other_field');
+    mockValidation1('');
+    mockValidation2('');
+    mockValidation3('');
+  });
+
   test('Should return empty if all validations return empty', () {
-    final validationEmpty = FieldValidationSpy();
-    final sut = ValidationComposite([validationEmpty]);
-
-    when(() => validationEmpty.field).thenReturn('any_field');
-    when(() => validationEmpty.field).thenReturn('');
-
     final error = sut.validate(field: 'any_field', value: 'any_value');
 
     expect(error, '');
