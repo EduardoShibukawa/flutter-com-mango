af3a28a12988e605597e466070a179ea1dc68de4
Eduardo Shibukawa <eduardo.shibukawa@thoughtworks.com>
2021-07-16 18:21:52 -0300

    move duplicate code to setUp method

--- clean_flutter/test/data/usecases/load_current_account/load_current_account_test.dart
+++ clean_flutter/test/data/usecases/load_current_account/load_current_account_test.dart
@@ -1,3 +1,4 @@
+import 'package:clean_flutter/domain/usecases/load_current_account.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
 
@@ -5,14 +6,18 @@ class FetchSecureCacheStorageSpy extends Mock
     implements FetchSecureCacheStorage {}
 
 void main() {
-  test('Should call FetchSecureCacheStorage with correct values', () async {
-    final fetchSecureCacheStorage = FetchSecureCacheStorageSpy();
-    final sut = LocalLoadCurrentAccount(
+  late LocalLoadCurrentAccount sut;
+  late FetchSecureCacheStorage fetchSecureCacheStorage;
+
+  setUp(() {
+    fetchSecureCacheStorage = FetchSecureCacheStorageSpy();
+    sut = LocalLoadCurrentAccount(
         fetchSecureCacheStorage: fetchSecureCacheStorage);
 
     when(() => fetchSecureCacheStorage.fetchSecure(any()))
         .thenAnswer((_) => Future.value());
-
+  });
+  test('Should call FetchSecureCacheStorage with correct values', () async {
     await sut.load();
 
     verify(() => fetchSecureCacheStorage.fetchSecure('token')).called(1);
