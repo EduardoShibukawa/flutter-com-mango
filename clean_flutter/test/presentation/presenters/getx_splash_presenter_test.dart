e3a450515fde5f604e885da9cec63445d6f33f85
Eduardo Shibukawa <eduardo.shibukawa@thoughtworks.com>
2021-07-19 11:24:02 -0300

    ensure GetxSplashPresenter emits surveys page on success

--- clean_flutter/test/presentation/presenters/getx_splash_presenter_test.dart
+++ clean_flutter/test/presentation/presenters/getx_splash_presenter_test.dart
@@ -24,6 +24,14 @@ void main() {
 
     verify(() => loadCurrentAccount.load()).called(1);
   });
+
+  test('Should go to surveys page on success', () async {
+    sut.navigateToStream.listen(expectAsync1(
+      (page) => expect(page, '/surveys'),
+    ));
+
+    await sut.checkAccount();
+  });
 }
 
 class GetxSplashPresenter implements SplashPresenter {
@@ -36,5 +44,6 @@ class GetxSplashPresenter implements SplashPresenter {
 
   Future<void> checkAccount() async {
     await loadCurrentAccount.load();
+    _navigateTo.value = '/surveys';
   }
 }
