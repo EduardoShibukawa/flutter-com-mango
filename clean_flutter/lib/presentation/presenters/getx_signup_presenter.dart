3b1926087b56520b2859af6eb28511fdcf1490bc
Eduardo Shibukawa <eduardo.shibukawa@thoughtworks.com>
2021-07-19 18:49:04 -0300

    ensure GetxSignUpPresenter emits correct error on email changed

---
+++ clean_flutter/lib/presentation/presenters/getx_signup_presenter.dart
@@ -0,0 +1,45 @@
+import 'dart:async';
+
+import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:get/state_manager.dart';
+
+import '../../ui/helpers/errors/errors.dart';
+import 'presenters.dart';
+
+class GetxSignUpPresenter extends GetxController {
+  final Validation validation;
+
+  var _emailError = Rxn<UIError>();
+  var _isFormValid = false.obs;
+
+  Stream<UIError?> get emailErrorStream => _emailError.stream;
+  Stream<bool> get isFormValidStream => _isFormValid.stream.map((s) => s!);
+
+  GetxSignUpPresenter({
+    required this.validation,
+  });
+
+  void validateEmail(String email) {
+    _emailError.value = _validateField(field: 'email', value: email);
+    _validateForm();
+  }
+
+  UIError? _validateField({required String field, required String value}) {
+    final error = validation.validate(field: field, value: value);
+
+    switch (error) {
+      case ValidationError.invalidField:
+        return UIError.invalidField;
+      case ValidationError.requiredField:
+        return UIError.requiredField;
+      default:
+        return null;
+    }
+  }
+
+  void _validateForm() {
+    _isFormValid.value = false;
+  }
+
+  void dispose();
+}
