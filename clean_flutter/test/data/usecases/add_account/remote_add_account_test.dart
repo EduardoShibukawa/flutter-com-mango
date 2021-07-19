50a8fe2a30630b042f8f47177a73e525b9733ce0
Eduardo Shibukawa <eduardo.shibukawa@thoughtworks.com>
2021-07-19 15:38:44 -0300

    ensure RemoteAddAccount throws UnexpectedError on 500

--- clean_flutter/test/data/usecases/add_account/remote_add_account_test.dart
+++ clean_flutter/test/data/usecases/add_account/remote_add_account_test.dart
@@ -82,4 +82,15 @@ void main() {
     // Assert
     expect(future, throwsA(DomainError.unexpected));
   });
+
+  test('Should throw UnexpectedError if HttpClient returns 500', () async {
+    // Arrange
+    mockHttpError(HttpError.serverError);
+
+    // Act
+    final future = sut.add(params: params);
+
+    // Assert
+    expect(future, throwsA(DomainError.unexpected));
+  });
 }
