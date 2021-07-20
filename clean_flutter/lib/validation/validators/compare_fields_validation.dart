6954c3fec305a0e6cc6127d443c6d7502e00509d
Eduardo Shibukawa <eduardo.shibukawa@thoughtworks.com>
2021-07-20 19:19:27 -0300

    add invalid cases test for CompareFieldsValidation

--- clean_flutter/lib/validation/validators/compare_fields_validation.dart
+++ clean_flutter/lib/validation/validators/compare_fields_validation.dart
@@ -8,6 +8,10 @@ class CompareFieldsValidation implements FieldValidation {
   CompareFieldsValidation({required this.field, required this.fieldToCompare});
 
   ValidationError? validate(Map input) {
+    if (input[field] == null || input[fieldToCompare] == null) {
+      return null;
+    }
+
     return input[field] == input[fieldToCompare]
         ? null
         : ValidationError.invalidField;
