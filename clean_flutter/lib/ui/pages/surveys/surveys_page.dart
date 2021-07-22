a6e82f46f3e536b3b671cef0701e8c32c67334c2
Eduardo Shibukawa <eduardo.shibukawa@thoughtworks.com>
2021-07-22 13:21:37 -0300

    improve SurveysPage layout

--- clean_flutter/lib/ui/pages/surveys/surveys_page.dart
+++ clean_flutter/lib/ui/pages/surveys/surveys_page.dart
@@ -35,14 +35,22 @@ class SurveysPage extends StatelessWidget {
             stream: presenter.surveysStream,
             builder: (context, snapshot) {
               if (snapshot.hasError) {
-                return Column(
-                  children: [
-                    Text(snapshot.error!.toString()),
-                    TextButton(
-                      onPressed: presenter.loadData,
-                      child: Text(R.strings.reload),
-                    ),
-                  ],
+                return Padding(
+                  padding: const EdgeInsets.all(40),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        snapshot.error!.toString(),
+                        style: TextStyle(fontSize: 16),
+                        textAlign: TextAlign.center,
+                      ),
+                      ElevatedButton(
+                        onPressed: presenter.loadData,
+                        child: Text(R.strings.reload),
+                      ),
+                    ],
+                  ),
                 );
               } else if (snapshot.hasData) {
                 return Padding(
