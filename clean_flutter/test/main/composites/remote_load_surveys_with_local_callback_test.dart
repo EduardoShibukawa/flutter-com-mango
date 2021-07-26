81160303f673e250470e256e910aebfb594ba6b3
Eduardo Shibukawa <eduardo.shibukawa@thoughtworks.com>
2021-07-26 10:55:41 -0300

    ensure Composite calls local save with remote data

--- clean_flutter/test/main/composites/remote_load_surveys_with_local_callback_test.dart
+++ clean_flutter/test/main/composites/remote_load_surveys_with_local_callback_test.dart
@@ -1,18 +1,42 @@
 import 'package:clean_flutter/data/usecase/usecase.dart';
+import 'package:clean_flutter/domain/entities/survey_entity.dart';
+import 'package:faker/faker.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
 
 class RemoteLoadSurveysSpy extends Mock implements RemoteLoadSurveys {}
 
+class LocalLoadSurveysSpy extends Mock implements LocalLoadSurveys {}
+
 void main() {
+  late LocalLoadSurveysSpy local;
   late RemoteLoadSurveysSpy remote;
   late RemoteLoadSurveysWithLocalFallback sut;
+  late List<SurveyEntity> surveys;
+
+  List<SurveyEntity> mockSurveys() => [
+        SurveyEntity(
+            id: faker.guid.guid(),
+            question: faker.randomGenerator.string(10),
+            dateTime: faker.date.dateTime(),
+            didAnswer: faker.randomGenerator.boolean())
+      ];
+
+  void mockRemoteLoad() {
+    surveys = mockSurveys();
+    when(() => remote.load()).thenAnswer((_) async => surveys);
+  }
+
+  void mockSaveLocal() =>
+      when(() => local.save(any())).thenAnswer((_) async => {});
 
   setUp(() {
     remote = RemoteLoadSurveysSpy();
-    sut = RemoteLoadSurveysWithLocalFallback(remote: remote);
+    local = LocalLoadSurveysSpy();
+    sut = RemoteLoadSurveysWithLocalFallback(remote: remote, local: local);
 
-    when(() => remote.load()).thenAnswer((_) async => []);
+    mockRemoteLoad();
+    mockSaveLocal();
   });
 
   test('Should call remote load', () async {
@@ -20,14 +44,26 @@ void main() {
 
     verify(() => remote.load()).called(1);
   });
+
+  test('Should call local save with remote data', () async {
+    await sut.load();
+
+    verify(() => local.save(surveys)).called(1);
+  });
 }
 
 class RemoteLoadSurveysWithLocalFallback {
   final RemoteLoadSurveys remote;
+  final LocalLoadSurveys local;
 
-  RemoteLoadSurveysWithLocalFallback({required this.remote});
+  RemoteLoadSurveysWithLocalFallback({
+    required this.remote,
+    required this.local,
+  });
 
   Future<void> load() async {
-    await remote.load();
+    final surveys = await remote.load();
+
+    await local.save(surveys);
   }
 }
