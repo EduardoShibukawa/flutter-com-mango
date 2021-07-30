f6f5bbc84682b143221a2183c6b823e73a825b17
Eduardo Shibukawa <eduardoshibuka@gmail.com>
2021-07-30 13:57:32 -0300

    prevent current answer click

--- clean_flutter/lib/ui/pages/survey_result/components/survey_result.dart
+++ clean_flutter/lib/ui/pages/survey_result/components/survey_result.dart
@@ -17,9 +17,12 @@ class SurveyResult extends StatelessWidget {
           return SurveyHeader(viewModel.question);
         }
 
+        final answer = viewModel.answers[index - 1];
+
         return GestureDetector(
-          onTap: () => onSave(answer: viewModel.answers[index - 1].answer),
-          child: SurveyAnswer(viewModel.answers[index - 1]),
+          onTap: () =>
+              answer.isCurrentAnswer ? null : onSave(answer: answer.answer),
+          child: SurveyAnswer(answer),
         );
       },
       itemCount: viewModel.answers.length + 1,
